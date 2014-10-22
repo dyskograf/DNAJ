d80024a4e32dcaac924b402d8fc21f7218697880
Michal Lewandowski <dyskograf@gmail.com>
2014-10-22 07:36:19 +0200

    class2 excercise completed

--- class2/ee.js
+++ class2/ee.js
@@ -1,49 +1,51 @@
-(function (global) {
-	var EE;
-
-	if (!global.UAM) {
-		global.UAM = {};
-	}
-
-	EE = function () {
-		//store the listeners somewhere
-		this.listeners = {};
-	};
-
-	EE.prototype.on = function (eventName, listener, context) {
-        this.listeners[eventName] = listener;
-        var currentListenersContext = this.listeners[eventName];
-        
-        currentListenersContext[currentListenersContext.length] = listener;
-        for (var key in context)
-        {
-            this.listeners.eventName.prototype.key = context.key;
+(function(global) {
+    var EE;
+    if (!global.UAM) {
+        global.UAM = {};
+    }
+    
+    EE = function() {
+        //store the listeners somewhere
+        this.listeners = {};
+    };
+    
+    EE.prototype.on = function(eventName, listener, context) {
+        if (!this.listeners[eventName]) {
+            this.listeners[eventName] = [];
         }
         
-        return function() { delete this.listeners[eventName] };
-	};
-
-	EE.prototype.emit = function (eventName /*, other args...*/) {
-        var currentListenersContext = this.listeners[eventName];
+        var events = this.listeners[eventName];
+        var binded = listener.bind(context);
+        events.push(binded);
         
-        for(var item in currentListenersContext)
-        {
-            item.apply(arguments);   
+        return function() {
+            var index = events.indexOf(binded);
+            if (index >= 0) {
+                events.splice(index, 1);
+            }
+        };
+    };
+    EE.prototype.emit = function(eventName) {
+        var copied_arguments = [];
+        
+        for (var x = 1; x < arguments.length; x++) {
+            copied_arguments.push(arguments[x]);
         }
-	};
-
-//	var ee = new EE();
-//
-//	var removeListener = ee.on('test', function (arg1, arg2) {
-//		console.log(arg1, arg2, this.key);
-//	}, { key: 'value' });
-//
-//	ee.emit('test', 1, 2); // 1, 2 value
-//
-//	removeListener(); //removes my listener from the event emitter;
-//
-//	ee.emit('test'); //nothing will execute
-
-	global.UAM.EventEmitter = EE;
-
+        
+        for (var item in this.listeners[eventName]) {
+            this.listeners[eventName][item].apply(this, copied_arguments);
+        }
+    };
+    // var ee = new EE();
+    //
+    // var removeListener = ee.on('test', function (arg1, arg2) {
+    // global.console.log(arg1, arg2, this.key);
+    // }, { key: 'value' });
+    //
+    // ee.emit('test', 1, 2); // 1, 2 value
+    //
+    // removeListener(); //removes my listener from the event emitter;
+    //
+    // ee.emit('test'); //nothing will execute
+    global.UAM.EventEmitter = EE;
 }(window));
